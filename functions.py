470d578bc2bdf8102f2d828ea9f12bef95185ef1
Johanna Rangel <johannarangel14@gmail.com>
2023-11-27 22:30:10 -0500

    commit

--- functions.py
+++ functions.py
@@ -1,40 +1,24 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[ ]:
+# ### Funciones para Endpoints / ML
+
+# En esta sección, nos centraremos en consumir los archivos que se generaron en el notebook **Consultas**. que serán utilizadas por la API. El objetivo principal es dar respuestas a los Endpoints y a los Modelos Machine Learning, garantizando rapidez y eficacia en la ejecución de la API.
+
+# In[6]:
 
 
 import pandas as pd
 import os
 
 
-# In[ ]:
+# ### Endpoint 1
 
-
-def UserForGenre(genero:str):
-    consulta2 = pd.read_csv('UserForGenre.csv')
-    
-    # Filtrar el DataFrame por el género dado
-    genre_data = consulta2[consulta2['genres'] == genero]
-
-    # Encontrar al usuario con más horas jugadas para ese género
-    top_user = genre_data.loc[genre_data['hours_game'].idxmax()]['user_id']
-
-    # Crear una lista de acumulación de horas jugadas por año
-    hours_by_year = genre_data.groupby('year')['hours_game'].sum().reset_index()
-    hours_by_year = hours_by_year.rename(columns={'year': 'Año', 'hours_game': 'Horas'})
-    # Convertir las horas a enteros (int) dividiendo por 60
-    #hours_by_year['Horas'] = (hours_by_year['Horas'] / 60).astype(int)
-    hours_list = hours_by_year.to_dict(orient='records')
-
-    # Crear el diccionario de retorno
-    result = {
-        "Usuario con más horas jugadas para Género {}".format(genero): top_user,
-        "Horas jugadas": hours_list
-    }
-
-    return result
-
+# **def PlayTimeGenre( genero : str ):** Debe devolver año con mas horas jugadas para dicho género.
+# 
+# Ejemplo de retorno: {"Año de lanzamiento con más horas jugadas para Género X" : 2013}
+# 
+# Input: **PlayTimeGenre.csv**
 
 # In[ ]:
 
@@ -58,6 +42,50 @@ def PlayTimeGenre(genero: str):
     return response_data
 
 
+# ### Endpoint 2
+
+# **def UserForGenre( genero : str ):** Debe devolver el usuario que acumula más horas jugadas para el género dado y una lista de la acumulación de horas jugadas por año.
+# 
+# Ejemplo de retorno: {"Usuario con más horas jugadas para Género X" : us213ndjss09sdf, "Horas jugadas":[{Año: 2013, Horas: 203}, {Año: 2012, Horas: 100}, {Año: 2011, Horas: 23}]}
+# 
+# Input: **UserForGenre.csv**
+
+# In[ ]:
+
+
+def UserForGenre(genero:str):
+    consulta2 = pd.read_csv('UserForGenre.csv')
+    
+    # Filtrar el DataFrame por el género dado
+    genre_data = consulta2[consulta2['genres'] == genero]
+
+    # Encontrar al usuario con más horas jugadas para ese género
+    top_user = genre_data.loc[genre_data['hours_game'].idxmax()]['user_id']
+
+    # Crear una lista de acumulación de horas jugadas por año
+    hours_by_year = genre_data.groupby('year')['hours_game'].sum().reset_index()
+  
+    hours_by_year = hours_by_year.rename(columns={'year': 'Año', 'hours_game': 'Horas'})
+    
+    hours_list = hours_by_year.to_dict(orient='records')
+
+    # Crear el diccionario de retorno
+    result = {
+        "Usuario con más horas jugadas para Género {}".format(genero): top_user,
+        "Horas jugadas": hours_list
+    }
+
+    return result
+
+
+# ### Endpoint 3
+
+# **def UsersRecommend( año : int )**: Devuelve el top 3 de juegos MÁS recomendados por usuarios para el año dado. (reviews.recommend = True y comentarios positivos/neutrales)
+# 
+# Ejemplo de retorno: [{"Puesto 1" : X}, {"Puesto 2" : Y},{"Puesto 3" : Z}]
+# 
+# Input: **UsersRecommend.csv**
+
 # In[ ]:
 
 
@@ -73,6 +101,14 @@ def UsersRecommend(year: int):
 
     return response_data
 
+
+# ### Endpoint 4
+
+# **def UsersWorstDeveloper( año : int )**: Devuelve el top 3 de desarrolladoras con juegos MENOS recomendados por usuarios para el año dado. (reviews.recommend = False y comentarios negativos)
+# 
+# Ejemplo de retorno: [{"Puesto 1" : X}, {"Puesto 2" : Y},{"Puesto 3" : Z}]
+# 
+# Input: **UsersWorstDeveloper.csv**
 
 # In[ ]:
 
@@ -91,6 +127,14 @@ def UsersWorstDeveloper(year: int):
 
 
 
+# ### Endpoint 5
+
+# **def sentiment_analysis( empresa desarrolladora : str )**: Según la empresa desarrolladora, se devuelve un diccionario con el nombre de la desarrolladora como llave y una lista con la cantidad total de registros de reseñas de usuarios que se encuentren categorizados con un análisis de sentimiento como valor.
+# 
+# Ejemplo de retorno: {'Valve' : [Negative = 182, Neutral = 120, Positive = 278]}
+# 
+# Input: **sentiment_analysis.csv**
+
 # In[ ]:
 
 
@@ -106,20 +150,26 @@ def sentiment_analysis(empresa_desarrolladora: str):
     return response_data
 
 
-# In[ ]:
+# ### ML
+
+# ### Recomendación item-item
+
+# In[7]:
 
 
 def recomendacion_usuario(item_id):
-    df = pd.read_csv('modelo_espec.csv')
-
+    df = pd.read_csv('recomienda_item_item.csv')
+    
     # Filtrar el DataFrame por el año especificado
     result_df = df[df['item_id'] == item_id]
     
-    response_data = result_df['RecomendacionesTop5']
-    '''
-    response_data = [{"Puesto 1": result_df.iloc[0]['developer']},
-                    {"Puesto 2": result_df.iloc[1]['developer']},
-                    {"Puesto 3": result_df.iloc[2]['developer']}]
-    '''
+    response_data = result_df['Recomendaciones']
+ 
     return response_data
+
+
+# In[ ]:
+
+
+
 
